dd7915fa95d81881c56e516dfc1061035e552130
stuser <stuser@192.168.1.50>
2019-06-30 15:59:09 -0500

    6/30/19

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Hello {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("What will your square be made of?");
+		String y = scan.next();
+		System.out.println("What will the width of your rectangle be?");
+		int z = scan.nextInt();
+		System.out.println("What will the length of your rectangle be?");
+		int r = scan.nextInt();
+		for(int i = 0; i<z; i++) {
+			for(int x = 0; x<r; x++) {
+				System.out.print(y + " ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ src/Triangle.java
@@ -0,0 +1,13 @@
+
+public class Triangle {
+	public static void main(String[] args) {
+		for(int i = 0; i<10; i++) {
+			for(int x = 0; x<i; x++) {
+				System.out.print(":^)");
+			}
+			System.out.println();
+
+		}
+	}
+
+}
