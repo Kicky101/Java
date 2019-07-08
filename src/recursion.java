e0a010318d6074805ae73606b58770df36f22069
SummerTech <stusr@summertech.net>
2019-07-08 09:59:29 -0400

    7-9-19

--- src/recursion.java
+++ src/recursion.java
@@ -3,7 +3,7 @@
  public class recursion { 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Do you want to break the code(1) count to 0(2) or do some factorials(3): ");
+        System.out.print("Do you want to break the code(1) count to 0(2) do some factorials(3) or add stuff(4): ");
         int first = scan.nextInt();
         if(first == 1) {
             printX();
@@ -18,6 +18,13 @@
             System.out.print("What number would you like to factorial: ");
             int inputNum1 = scan.nextInt();
             System.out.println(factorial(inputNum1));
+        }
+        if(first == 4) {
+            System.out.print("What will your first number be: ");
+            int firstNum = scan.nextInt();
+            System.out.print("What will your second number be: ");
+            int secondNum = scan.nextInt();
+            System.out.println(add(firstNum, secondNum));
         }
         
     }
@@ -42,9 +49,15 @@
         } 
     }
     private static int factorial(int inputNum1) {
-        if(inputNum1-1 == 0) {
-            return factorial(inputNum1); 
+        if(inputNum1 == 1) {
+            return inputNum1; 
         }
-        return factorial(inputNum1*factorial(inputNum1-1));
+        return inputNum1*factorial(inputNum1-1);
+    }
+    private static int add(int firstNum, int secondNum) {
+        if(firstNum-1+0 == secondNum){
+            return firstNum;
+        } 
+        return 1+add(firstNum, secondNum);
     }
  }
