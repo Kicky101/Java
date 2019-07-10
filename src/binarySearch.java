7b3e8e79b3a6f4d1e6fbcbfb989d7283211e463d
SummerTech <stusr@summertech.net>
2019-07-10 09:59:06 -0400

    7-10-19

--- src/binarySearch.java
+++ src/binarySearch.java
@@ -1,25 +1,32 @@
 import java.util.Scanner;
+
 import java.util.Arrays;
+
+import java.util.Random;
 
 public class binarySearch {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
         int[] bruh = new int[11];
         for(int i = 1; i<=10; i++) {
-            bruh[i] = 2*i;
+            bruh[i] = rand.nextInt(50);
         }
+        for(int i = 0; i<bruh.length; i++) {
+			System.out.println(bruh[i]);
+        }
+        /*
         for(int i=0;i<bruh[bruh.length-1];i++) {
             System.out.print("What number will you look for: ");
         int num = i;
         System.out.println(search(num, bruh));
-        /*
         System.out.print("Do you want to exit: ");
         String exit = scan.next();
         if(exit.toLowerCase().equals("yes")) {
             break;
         }
         */
-        }
+        
     }
     private static boolean search(int num, int[] bruh) {
             if(bruh.length == 1) {
@@ -37,5 +44,10 @@ public class binarySearch {
             else {
                 return false;
             }
+    }
+    private static int[] insertionSort(int[] bruh) {
+        int endRedacted = 0;
+        int[] what = new int[bruh.length];
+         
     }
 }
