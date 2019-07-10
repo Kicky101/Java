bad12ca1f0701e63d89bcf6e67d03cbe5398d016
SummerTech <stusr@summertech.net>
2019-07-10 13:29:30 -0400

    7-10-1999

--- src/binarySearch.java
+++ src/binarySearch.java
@@ -8,30 +8,39 @@ public class binarySearch {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Random rand = new Random();
-        int[] bruh = new int[11];
-        for(int i = 1; i<=10; i++) {
+        int[] bruh = new int[rand.nextInt(20)+1];
+        for(int i = 0; i<bruh.length; i++) {
             bruh[i] = rand.nextInt(50);
         }
+        System.out.println("");
+        System.out.println("Unsorted");
+        System.out.println("");
+        for(int i = 0; i<bruh.length; i++) {
+			System.out.println(bruh[i]);
+        }
+        System.out.println("");
+        System.out.println("Sorted");
+        System.out.println("");
         bruh = selectionSort(bruh);
         for(int i = 0; i<bruh.length; i++) {
 			System.out.println(bruh[i]);
         }
-        /*
-        for(int i=0;i<bruh[bruh.length-1];i++) {
+        System.out.println("");
+        while(true) {
             System.out.print("What number will you look for: ");
-        int num = i;
-        System.out.println(search(num, bruh));
-        System.out.print("Do you want to exit: ");
-        String exit = scan.next();
+            int num = scan.nextInt();
+            System.out.println(search(num, bruh));
+            System.out.print("Do you want to exit: ");
+            String exit = scan.next();
         if(exit.toLowerCase().equals("yes")) {
             break;
         }
-        */
+    }
         
     }
     private static boolean search(int num, int[] bruh) {
             if(bruh.length == 1) {
-                return bruh.length == num;
+                return (bruh[0] == num);
             }
             else if(num == bruh[bruh.length/2]) {
                 return true;
@@ -48,17 +57,30 @@ public class binarySearch {
     }
     private static int[] selectionSort(int[] bruh) {
         int[] what = new int[bruh.length];
+        if(bruh.length == 0) {
+            return what;
+        }
         int deletThis = bruh[0];
         for(int i = 1; i<bruh.length; i++){
             if(bruh[i]<deletThis) {
                 deletThis = bruh[i];
+                int bruhMoment = bruh[i];
+                bruh[i] = bruh[0];
+                bruh[0] = bruhMoment;
             }
         }
         what[0] = deletThis;
+
+
+
         int[] x = selectionSort(Arrays.copyOfRange(bruh, 1, bruh.length));
         for(int i = 1; i<what.length; i++) {
             what[i] = x[i-1];
         }
         return what;
+    }
+    private static int[] mergeSort(int[] bruh) {
+        int[] what = new int[bruh.length];
+        
     }
 }
