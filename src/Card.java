484e2c4b2ec3bc33cde5cc2ccabef30dafb7d56b
hucki <hucki@LAPTOP-AGMDRPE6>
2020-06-22 12:01:42 -0400

    6/22/2020

---
+++ src/Card.java
@@ -0,0 +1,13 @@
+
+public class Card {
+	private String data;
+	public Card(String ok) {
+		data = ok;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String no) {
+		data = no;
+	}
+}
---
+++ src/cardTest.java
@@ -0,0 +1,8 @@
+
+public class cardTest {
+	public static void main(String[] args) {
+		Card deck = new Card("yes");
+		
+		System.out.print(deck.getData());
+	}
+}
