a162f4a284033c9f71b42d1148ac08b5ec1b35a5
hucki <hucki@LAPTOP-AGMDRPE6>
2020-07-10 09:56:52 -0400

    7/10/20 1

--- src/Tree.java
+++ src/Tree.java
@@ -1,4 +1,27 @@
 
 public class Tree {
-	
+	private confusingNode headNode;
+	public Tree() {
+		
+	}
+	public void add(int x) {
+		if(headNode == null) {
+			headNode = new confusingNode(x);
+		}
+		else {
+			addRecursion(x, headNode);
+		}
+	}
+	public void addRecursion(int x, confusingNode newNode) {
+		if(newNode.getRightNode() == null) {
+			if(x > newNode.getRightNode().getData()) {
+				// add	
+			}
+		}
+		else if(newNode.getLeftNode() == null) {
+			if(x <= newNode.getLeftNode().getData()) {
+				// add
+			}
+		}
+	}
 }
