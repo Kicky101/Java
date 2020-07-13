6cb95237e182b1f93101831ba43cfe18ee28af95
hucki <hucki@LAPTOP-AGMDRPE6>
2020-07-13 10:01:19 -0400

    7/13/20 1

--- src/Tree.java
+++ src/Tree.java
@@ -1,6 +1,7 @@
 
 public class Tree {
 	private confusingNode headNode;
+	public Tree Tree;
 	public Tree() {
 		
 	}
@@ -39,19 +40,30 @@ public class Tree {
 		}
 	}
 	public String toString() {
-		confusingNode newNode = new confusingNode(0);
+		return recursiveToString(headNode);
+	}
+	public String recursiveToString(confusingNode newNode) {
 		String temp = "";
 		if(newNode.getRightNode() != null && newNode.getLeftNode() == null) {
 			// only one node on the right
+			recursiveToString(newNode = newNode.getLeftNode());
+			temp = "This node contains " + newNode.getData();
 		}
 		else if(newNode.getRightNode() == null && newNode.getLeftNode() != null) {
 			// only one node on the left
+			recursiveToString(newNode = newNode.getRightNode());
+			temp = "This node contains " + newNode.getData();
 		}
 		else if(newNode.getRightNode() != null && newNode.getLeftNode() != null) {
 			// one node on both right and left
+			recursiveToString(newNode = newNode.getRightNode());
+			temp = "This node contains " + newNode.getData();
+			recursiveToString(newNode = newNode.getLeftNode());
+			temp = "This node contains " + newNode.getData();
 		}
 		else if(newNode.getRightNode() == null && newNode.getLeftNode() == null) {
 			// no nodes on both right and left
+			return temp;
 		}
 		return temp;
 	}
---
+++ src/treeNodeTest.java
@@ -0,0 +1,14 @@
+
+public class treeNodeTest {
+	public static void main(String[] args) {
+		Tree Tree = new Tree();
+		
+		Tree.add(3);
+		
+		Tree.add(2);
+		
+		Tree.add(5);
+		
+		System.out.println(Tree.toString());
+	}
+}
