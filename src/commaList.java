3effcb2d6418946e9b7bfc7fb7bf8303ca74da29
hucki <hucki@LAPTOP-AGMDRPE6>
2020-12-29 12:01:37 -0500

    12/29/2020 2

--- src/commaList.java
+++ src/commaList.java
@@ -4,32 +4,42 @@ import java.util.Scanner;
 public class commaList {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		
 		ArrayList<String> commaList = new ArrayList<String>();
 		System.out.print("Enter things separated by commas: ");
-		String listBeta = scan.nextLine();
-		String list = removeWhiteSpaces(listBeta).toLowerCase();
+		String list = scan.nextLine();
 		int counter = 0;
-		int commaNumber = 0;
+		String tempString = "";
+		Scanner scann = new Scanner(list);
 		for(int i = 0; i<list.length(); i++) {
 			if(list.charAt(counter) == ',') {
-				commaNumber++;
+				if(counter < list.length()) {
+					if(list.charAt(counter + 1) == ' ') {
+						int start = counter + 1;
+						int end = counter + 2;
+						list = list.substring(start-1, end);
+					}
+				}
+				if(counter > 0) {
+					if(list.charAt(counter - 1) == ' ') {
+						int start = 0;
+						int end = counter - 2;
+						list = list.substring(start-1, end);
+					}
+				}
 			}
 			counter++;
+			System.out.println();
+			System.out.print(list.charAt(counter));
 		}
-		System.out.println(commaNumber);
-		counter = 0;
-		for(int i = 0; i<commaNumber; i++) {
-			if(list.charAt(counter) == ',') {
-				
-			}
+		scann.useDelimiter(",");
+		while(scann.hasNext()) {
+			commaList.add(scann.next());
 		}
-		
-		
-		
+		System.out.println();
+		System.out.println(commaList);
 		scan.close();
+		scann.close();
 	}
 	
-	private static String removeWhiteSpaces(String input) {
-        return input.replaceAll("\\s+", "");
-    }
 }
