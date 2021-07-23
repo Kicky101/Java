3daebf426f689c207540f6707a55d3d65cf8c111
hucki <hucki@LAPTOP-AGMDRPE6>
2021-07-23 14:07:43 -0400

    7/23/2021

--- src/heap.java
+++ src/heap.java
@@ -1,6 +1,48 @@
+import java.util.Scanner;
+
 public class heap {
 	public static void main(String[] args) {
-		
-		
+		heapClass heap = new heapClass();
+		Scanner scan = new Scanner(System.in);
+		boolean loop = true;
+		while(loop) {
+			System.out.print("Do you want to add, remove, or quit: ");
+			String control = scan.next();
+			if(control.toLowerCase().equals("add")) {
+				System.out.println();
+				System.out.print("What non-zero integer do you want to add: ");
+				String item = scan.next();
+				if(item.matches(".*\\d.*")) {
+					int number = Integer.parseInt(item);
+					if(number != 0) {
+						heap.add(number);
+						System.out.println();
+						System.out.println(heap.toString());
+					}
+					else {
+						System.out.println();
+						System.out.println("That is not a valid input");
+						System.out.println();
+					}
+				}
+				else {
+					System.out.println();
+					System.out.println("That is not a valid input");
+					System.out.println();
+				}
+			}
+			else if(control.toLowerCase().equals("remove")) {
+				
+			}
+			else if(control.toLowerCase().equals("quit")) {
+				loop = false;
+			}
+			else {
+				System.out.println();
+				System.out.println("That is not a valid input");
+				System.out.println();
+			}
+		}
+		scan.close();
 	}
 }
--- src/heapClass.java
+++ src/heapClass.java
@@ -1,21 +1,32 @@
+import java.util.Arrays;
+
 public class heapClass {
 	private int size = 10;
 	private int[] theHeap = new int[size];
-	private int last = 0;
+	private int open = 0;
 	public void add(int x) {
 		if(theHeap[0] == 0) {
 			theHeap[0] = x;
-			last++;
+			open++;
 		}
 		else {
-			theHeap[last] = x;
-			last++;
+			theHeap[open] = x;
+			open++;
 		}
 	}
 	public void remove() {
 		
 	}
-	public void sift() {
-		
+	public void siftUp(int x) {
+		int y = 0;
+		int placeholder = 0;
+		if(x > theHeap[y]) {
+			placeholder = theHeap[y];
+			theHeap[y] = x;
+			x = placeholder;
+		}
+	}
+	public String toString() {
+		return Arrays.toString(theHeap) + "\n";
 	}
 }
